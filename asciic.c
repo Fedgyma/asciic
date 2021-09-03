46fa24a41c7e2323aeaeb00c6aa11a99f3bfff86
Fedgyma <fedgyma@gmail.com>
2021-09-03 13:01:15 +0200

    initial

---
+++ asciic.c
@@ -0,0 +1,112 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int argc, char **argv)
+{
+	FILE *fd_in;
+	FILE *fd_out;
+	char *in_name;
+	char *out_name;
+	char *usage;
+
+	usage = "USAGE: <input ascii text> <output header>\n";
+	
+	if (argc != 3 ) {
+		printf(usage);
+		return 1;
+	}
+
+	in_name = argv[1];
+	out_name = argv[2];
+	
+	fd_in = fopen(in_name, "r");
+	fd_out = fopen(out_name, "w");
+
+	char *begin_header;
+	char *end_header;
+	char *var_def;
+	char *text = NULL;
+	char *end_null;
+	
+	begin_header = "#ifndef ASCII_ART_H\n#define ASCII_ART_H\n\n";
+	end_header = "\n\n#endif";
+	var_def = "const char ascii_art[] = {";
+	end_null = "0x00};";
+
+	char *buffer = 0;
+	long length;
+	
+	if (fd_in)
+	{
+		fseek(fd_in, 0, SEEK_END);
+		length = ftell(fd_in);
+		fseek(fd_in, 0, SEEK_SET);
+		buffer = malloc(length);
+		if (buffer)
+		{
+			fread(buffer, 1, length, fd_in);
+		}
+		fclose(fd_in);
+	}
+	
+	
+	char tmp[length*6];
+	int i;
+	int count;
+	int count2;
+	int count3;
+	
+	count = 80 - strlen(var_def);
+	count3 = 0;
+	count2 = 0;
+	if (strlen(var_def) <= 80) {
+		while(count > 6) {
+			count -= 6;
+			count3++;
+		}
+	}
+
+	for (i = 0; i < length; i++) {
+		
+		if (count3 > 0) {
+			count3--;
+			sprintf(tmp + i*6, "0x%02x, ", buffer[i]);
+		}
+		else {
+			break;
+		}
+	}
+
+	tmp[strlen(tmp)-1] = 0x0a;
+	
+	for (; i < length; i++) {
+		count2 += 6;
+		if (count2 < 72) {
+			sprintf(tmp + i*6, "0x%02x, ", buffer[i]);
+		
+		} else {
+			sprintf(tmp + i*6, "0x%02x,\n", buffer[i]);
+			count2 = 0;
+		}
+
+	}
+	size_t text_len = strlen(begin_header) + strlen(var_def)\
+		      + strlen(tmp) + strlen(end_header) + strlen(end_null); 
+	
+	text = (char*) malloc(text_len + 1);
+	strcpy(text, begin_header);
+	strcat(text, var_def);
+	strcat(text, tmp);
+	strcat(text, end_null);
+	strcat(text, end_header);
+	fputs(text, fd_out);
+	
+	free(text);	
+	
+	fclose(fd_out);
+
+	return 0;
+}
